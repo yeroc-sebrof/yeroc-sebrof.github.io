b9ad7691e4a1bd676ba9143493d8f722021b880d
Corey <1500812@abertay.ac.uk>
2018-06-28 16:50:21 +0100

    Added loader for contents
    Header and Footer loader

--- assets/element.js
+++ assets/element.js
@@ -1,12 +1,16 @@
 //JQ
 $(document).ready(function () {
-    "use strict";
-    $(".contact").hide();
+	"use strict";
+	$(".contact").hide();
+	$("#gHeader").load("/assets/navbar.html");
+	$("#gHeader").load("/assets/navbar.html");
+	
 	$("#contact").click(function () {
-        $(".contact").toggle(500);
-        $('html, body').animate({scrollTop: '+=400px'}, 450);
+		$(".contact").toggle(500);
+		$('html, body').animate({scrollTop: '+=400px'}, 450);
 	});
-    $("#headerPadding").click(function () {
-        $('html, body').animate({scrollTop: '+=' + window.innerHeight + 'px'}, 450);
+	
+	$("#headerPadding").click(function () {
+		$('html, body').animate({scrollTop: '+=' + window.innerHeight + 'px'}, 450);
 	});
 });
