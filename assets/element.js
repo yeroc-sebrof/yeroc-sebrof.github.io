5c7293e5e0f67d47a193a5f557f27d2211643b94
yeroc-sebrof <coreyforbes98@gmail.com>
2019-12-25 12:55:50 +0000

    Tab to Spaces & trying to fix navbar for about The great tab purge of 2019. Will check how this reflects on prod and if I have any JS or CSS issues from this before continuing

--- assets/element.js
+++ assets/element.js
@@ -1,18 +1,18 @@
 //JQ
 $(document).ready(function () {
-	"use strict";
-	$("#gHeader").load("/assets/navbar.html");
+    "use strict";
+    $("#gHeader").load("//assets/navbar.html");
 
-	$(".contact").hide();
+    $(".contact").hide();
 
-	$("#contact").click(function () {
-		$(".contact").toggle(500);
-		$('html, body').animate({scrollTop: '+=400px'}, 450);
-	});
+    $("#contact").click(function () {
+        $(".contact").toggle(500);
+        $('html, body').animate({scrollTop: '+=400px'}, 450);
+    });
 
-	$("#headerPadding").click(function () {
-		document.getElementById('contentStart').scrollIntoView({
- 			behavior: 'smooth'
-		});
-	});
+    $("#headerPadding").click(function () {
+        document.getElementById('contentStart').scrollIntoView({
+             behavior: 'smooth'
+        });
+    });
 });
