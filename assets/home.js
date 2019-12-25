b5900960ffa5e2f76d63fc1baf96ae135fbf7558
yeroc-sebrof <coreyforbes98@gmail.com>
2019-12-25 22:31:34 +0000

    Collapsable project content added

--- assets/home.js
+++ assets/home.js
@@ -1,8 +1,24 @@
 //JQ
 $(document).ready(function () {
+    var coll = document.getElementsByClassName("collapsible");
+
     $("#headerPadding").click(function () {
         document.getElementById('contentStart').scrollIntoView({
             behavior: 'smooth'
         });
     });
+
+    /// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible_symbol
+    var i;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
 });
